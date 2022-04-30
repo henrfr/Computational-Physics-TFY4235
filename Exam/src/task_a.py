b375f9f678c5a1a59c13a4798155092ab7040491
Henrik Friis <henrik.friis@outlook.com>
2022-04-30 17:12:59 +0200

    a and b completely done

--- Exam/src/task_a.py
+++ Exam/src/task_a.py
@@ -1,75 +1,59 @@
 from plot_params import set_plot_parameters
 import matplotlib.pyplot as plt
 import numpy as np
-from equations import normalize, F
-from solve_ode import heun, evolve_spins_old_but_working, evolve_spins
+from equations import normalize
+from solve_ode import evolve_spins_old, evolve_spins
 import time
-
-# def evolve_spins(data, N_steps, delta_t, mu, d_z, e_z, B, J, alpha, k_b, T, gamma, shape=(1,1)):
-#     # For each time step
-#     for i in range(N_steps-1):
-#         # For every value in row
-#         for row_val in range(1,shape[0]+1):
-#             # For every value in column
-#             for col_val in range(1,shape[1]+1):
-#                 # Find current spin values
-#                 S_j = data[i][row_val][col_val]
-
-#                 # Find the neighbour spin values, since the data is padded, it generalizes to 0D/1D
-#                 neighbours = np.array([data[i][row_val][col_val+1],  # Down
-#                                        data[i][row_val][col_val-1],  # Up
-#                                        data[i][row_val+1][col_val],  # Right
-#                                        data[i][row_val-1][col_val]]) # Left
-
-#                 # Find the effective field at current position
-#                 F_j = F(mu, S_j,d_z,e_z,B,J,neighbours,alpha,k_b,T,gamma,delta_t)
-
-#                 # Perform a step with Heun
-#                 data[i+1][row_val][col_val] = heun(delta_t, S_j, F_j, gamma, alpha)
-#     return data
 
 def task_a():
     gamma = 0.176 # 1.76*10**-1 T^-1 ps^-1 # /(T* ps)
-    J = 1 # meV Should be zero, but there are no neighbours so doesn't matter
     d_z = 0.1 # meV
     mu = 0.05788 # 5.788*10**-2 # meV*T^-1 # meV/T
     B_0 = 1.72 # T
     delta_t = 0.001 # 1*10**-3 # ps
     k_b = 0.08617 # 8.617*10**-2 # meV K^-1 # meV/K
-    alpha = 0 # 0.05
-    T = 0
+    alpha = 0 # Undamped
+    T = 0 # K
     B = np.array([0,0,B_0])
-    #B = np.array([0,0,0])
     e_z = np.array([0,0,1])
-    J = 0
-    d_z = 0 # Will only plot with one effect
+    J = 0 # meV
+
+    # Keep one of these to use only 1 symmetry-breaking term
+    d_z = 0 
+    #B = np.array([0,0,0])
 
 
-    N = 40000
+    N = 100000
     sim_time = N*delta_t
     N_steps = int(sim_time/delta_t)
 
     # Initializes with padding
     data = np.zeros(shape=(N_steps, 3, 3, 3))
 
-    # Initialize first state
-    data[0][1][1] = normalize(np.array([1,0,2]))
+    # Initialize first state. (it will be in the center of the padding)
+    data[0][1][1] = normalize(np.array([1,0,5]))
 
+    # Solve the ODE
     print("Start")
     start = time.time()
     data = evolve_spins(data, N_steps, delta_t, mu, d_z, e_z,
             B, J, alpha, k_b, T, gamma)
     print(f"End: {time.time() -start:.2f} seconds.")
     print(data.shape[0])
-    t = np.arange(data.shape[0])
+    t = np.arange(data.shape[0])*delta_t
 
-    x = data[:,1,1,0]
-
-    set_plot_parameters()
-    plt.plot(t, x, label=r"$S_x$")
-    plt.plot(t, data[:,1,1,1], label=r"$S_y$")
-    plt.plot(t, data[:,1,1,2], label=r"$S_z$")
-    plt.legend()
+    # Plotting
+    set_plot_parameters(20,20)
+    fig, axs = plt.subplots(figsize=(8,6))
+    axs.plot(t, data[:,1,1,0], label=r"$S_x$")
+    axs.plot(t, data[:,1,1,1], label=r"$S_y$")
+    axs.plot(t, data[:,1,1,2], label=r"$S_z$")
+    axs.set_xlabel("Time (ps)")
+    axs.set_ylabel("Spin value")
+    axs.legend()
+    fig.tight_layout()
+    fig.savefig("../plots/task_a.png", dpi=300)
     plt.show()
 
-task_a()
+if __name__ == "__main__":
+    task_a()
